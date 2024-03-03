8b8f1aa9039a45f44bede5ffb3f2d3598805db82
Jas-zzyy <159576592+Jas-zzyy@users.noreply.github.com>
2024-03-03 18:54:11 +0300

    Create user_input.py
    Plp School python assignment

---
+++ user_input.py
@@ -0,0 +1,14 @@
+# user_input.py
+
+# Ask for the user's name and store it in a variable called "name"
+name = input("Jasmine ")
+
+# Ask for the user's age and store it in a variable called "age"
+age = int(input("23 "))
+
+# Ask for the user's location and store it in a variable called "location"
+location = input("Kitengela")
+
+# Print out a personalized message using the user's name, age, and location
+print(f"Hello am {name}, I am{age} years old and live in {location}.")
+
